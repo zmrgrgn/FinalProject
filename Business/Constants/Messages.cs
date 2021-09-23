15dac968b4aad86226477fc02cf2fb2589eac25e
zumragirgin <zumragirgin@ogr.sakarya.edu.tr>
2021-09-23 10:40:51 +0300

    10. Gün Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürününüz eklendi.";
+        public static string ProductNameInvalid = "Ürün adı hatalı.";
+        public static string ProductsListed = "ürünler listelendi";
+        public static string MaintenanceTime = "sistem bakımda";
+    }
+}
